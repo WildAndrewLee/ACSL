e0805883fad5cff367bfd60770c36f51ff550c9b
AndrewProLee <me@andrewleenj.com>
2013-07-11 18:52:00 -0400

    Testing ST3 integration lulz

--- cells.py
+++ cells.py
@@ -1,5 +1,5 @@
 # Testing Git integration in ST3 lulz.
-
+fadsfsdfdfsd
 for l in open('INPUT'):
 	l=l.rstrip().split(', ')
 	if l[0][0]=='D':print(''.join(sorted(l[1][:4]))*2)+' and '+(''.join(sorted(l[1][4:]))*2)
